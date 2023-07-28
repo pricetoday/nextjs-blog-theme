9f0d0c33be301bc6df3c6e32708ca27766c67f65
Price Today <9403840+pricetoday@users.noreply.github.com>
2023-07-29 00:42:17 +0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Samsung Store Pakistan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Leading Samsung Mobile Phones Store in Pakistan';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Address: Fazl-e-Haq Plaza, Blue Area, Islamabad - Contact: 051-5289754';
 
   return {
     name,
